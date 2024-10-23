7d67d682360c762b019845301fa3f72002c7ac07
Joey Cruz <typicaltony0824@gmail.com>
2024-10-24 01:54:27 +0800

    added search files and creating the api

---
+++ script/api-data.js
@@ -0,0 +1,4 @@
+let ts = "1729702914978";
+let publicKey = "ac8dbe9a94779d909ed8e13500fd1e6d";
+let privateKey = "8dabef4d319039d346ed99c9adb90c99a0dc74f2";
+let hashVal = "076e10abbeafe8c0394dd7ca6808618f" 
