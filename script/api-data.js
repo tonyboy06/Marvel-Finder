85e12f525e38fc63a1519ae313ff58423a5f0b19
Joey Cruz <typicaltony0824@gmail.com>
2024-10-28 00:50:50 +0800

    fix api-data.js

--- script/api-data.js
+++ script/api-data.js
@@ -1,4 +1,3 @@
-let ts = "1729702914978";
+let ts = "1729869555371";
 let publicKey = "ac8dbe9a94779d909ed8e13500fd1e6d";
-let privateKey = "8dabef4d319039d346ed99c9adb90c99a0dc74f2";
-let hashVal = "076e10abbeafe8c0394dd7ca6808618f" 
+let hashVal = "a3684314d3f8d5fef94228823d85e0e2";
